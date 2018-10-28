67ae215abc397e2e1951a176b68c8146d5a2ef9b
Joaquim Barreto <joaquim@barreto.co>
2018-10-28 15:44:00 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,8 +31,8 @@ end
 
 def initial_round
   # code #initial_round here
-  puts display_card_total = deal_card + deal_card
-  puts display_card_total
+  puts card_total = deal_card + deal_card
+  return display_card_total
 end
 
 def hit?
